852c67514360915947e713151598677857e7de7d
Munem Habib <Munem.habib1@gmail.com>
2025-06-15 23:27:41 +0500

    fixation

--- mongoClient.js
+++ mongoClient.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://munemhabib1:lqt4hHQNwPXXPRTK@messaging.tatknda.mongodb.net/messaging_app?retryWrites=true&w=majority&appName=Messaging";
+const uri = process.env.MONGODB_URI
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
